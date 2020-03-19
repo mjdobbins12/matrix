b84729248f4e1b7790eb593fb2ef210b19e60ee7
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-19 21:44:02 +0000

    implicit classes ftw

--- matrcomp/src/Main.scala
+++ matrcomp/src/Main.scala
@@ -1,5 +1,19 @@
 package matrcomp
 
+object VecMeth {
+    implicit class VectorMethods(x: Vector[Int]) {
+        def dotProduct(y: Vector[Int]): Int = {
+            val elements = x zip y
+            elements.map(el => el._1 * el._2).fold(0)(_ + _)
+        }
+    }
+}
+
 object Main extends App {
-  println("Operational up in here")
+    import VecMeth._
+    val m = Vector(1, 2, 3)
+    val n = Vector(4, 5, 6)
+    val res = m dotProduct n
+
+    println(res)
 }
