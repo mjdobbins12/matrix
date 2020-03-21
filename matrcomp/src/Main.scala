46efd26dad95da9219bc9710666c98a8e7bced59
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-21 18:43:15 +0000

    hadamard product

--- matrcomp/src/Main.scala
+++ matrcomp/src/Main.scala
@@ -2,17 +2,22 @@ package matrcomp
 
 object VectorMethods {
     implicit class VecMeth(x: Vector[Int]) {
+        
         def +(y: Vector[Int]): Vector[Int] = {
             (x, y).zipped.map(_ + _)
         }
 
+        // multiplication of a vector by a scalar
         def *(a: Int): Vector[Int] = {
             x.map(_ * a)
         }
 
+        def hadamard(y: Vector[Int]): Vector[Int] = {
+            (x, y).zipped.map((xi, yi) => xi * yi)
+        }
+
         def dotProduct(y: Vector[Int]): Int = {
-            val elements = x zip y
-            elements.map(el => el._1 * el._2).fold(0)(_ + _)
+            (x hadamard y).fold(0)(_ + _)
         }
 
         def saxpy(a: Int, y: Vector[Int]): Vector[Int] = {
@@ -21,11 +26,17 @@ object VectorMethods {
     }
 }
 
+object MatrixMethods {
+    implicit class MatMeth(m: Vector[Vector[Int]]) {
+        def gaxpy = ???
+    }
+}
+
 object Main extends App {
     import VectorMethods._
     val m = Vector(1, 2, 3)
     val n = Vector(4, 5, 6)
-    val res = m.saxpy(3, n)
+    val res = m hadamard n
 
     println(res)
 }
